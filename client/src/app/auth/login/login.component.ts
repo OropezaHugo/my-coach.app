2b821f2673b646e7bd45ab310914a1b629294d45
Hugo Oropeza <oropezahugo35@gmail.com>
2025-03-17 13:07:01 -0400

    feat: first deploy configurations without https nor service worker

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,14 +1,13 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {UserService} from '../../services/user.service';
-import {NgOptimizedImage} from '@angular/common';
 import {GoogleUserInfo} from '../../models/auth.models';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
   imports: [
     MatButton,
-    NgOptimizedImage
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -16,12 +15,13 @@ import {GoogleUserInfo} from '../../models/auth.models';
 export class LoginComponent implements OnInit{
   userService = inject(UserService)
   tokenData: GoogleUserInfo | undefined = undefined;
+  BaseUrl = environment.baseUrl;
   ngOnInit() {
     this.tokenData = this.userService.getTokenData()
   }
 
   loginWithGoogle() {
-    window.location.href = 'http://localhost:5050/Auth/login'; // Backend de .NET
+    window.location.href = `${this.BaseUrl}/Auth/login`; // Backend de .NET
   }
 
   logout() {
--- client/src/app/services/prizes.service.ts
+++ client/src/app/services/prizes.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {environment} from '../../environments/environment.development';
+import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {AddPrizeToUserData, PrizeData, PrizeModel, UserPrizeInfoModel} from '../models/prize.models';
 
